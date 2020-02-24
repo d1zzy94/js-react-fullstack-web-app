0bba0603a47a72e75e0c0195ce6dc7958d7f97dc
dizzyPC <michal.studzinski94@gmail.com>
2020-02-24 20:20:08 +0100

    add table to department

--- employee-app/src/components/Department.js
+++ employee-app/src/components/Department.js
@@ -1,14 +1,46 @@
 import React, {Component} from 'react';
+import {Table} from 'react-bootstrap';
 
 export class Department extends Component 
 {
 
-    render()
-    {
+    constructor(props){
+        super(props);
+        this.state ={deps:[]}
+    }
+
+    componentDidMount(){
+        this.refreshList();
+    }
+
+    refreshList(){
+        this.setState({
+            deps:[{"DepartmentID":1, "DepartmentName":"IT"},
+            {"DepartmentID":2, "DepartmentName":"Sup"}
+        ]
+        })
+    }
+
+    render(){
+        const {deps} = this.state;
         return (
-            <div className="mt-5 d-flex jsutify-content-left">
-                <h3>This is the Department page.</h3>
-            </div>
-        );
+            <Table className="mt-4" striped bordered hover size="sm">
+                <thead>
+                    <tr>
+                        <th>DepartmentID</th>
+                        <th>DepartmentName</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {deps.map(dep=>
+                        <tr key = {dep.DepartmentID}>
+                        <td>{dep.DepartmentID}</td>
+                        <td>{dep.DepartmentName}</td>
+                        </tr>
+                        )}
+                </tbody>
+
+            </Table>
+        )
     }
 }

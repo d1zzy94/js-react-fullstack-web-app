c263f6c5a9869360987386e5cca6ba9ddc5cf5d7
dizzyPC <michal.studzinski94@gmail.com>
2020-03-19 19:22:23 +0100

    edit button

---
+++ employee-app/src/components/EditDepModal.js
@@ -0,0 +1,129 @@
+import React, {Component} from 'react';
+import {Modal, Button, Row, Col, Form} from 'react-bootstrap';
+
+import Snakbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton'
+
+export class EditDepModal extends Component{
+  constructor(props){
+      super(props);
+      this.state = {snackbaropen: false, snackbarmsg: ''};
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    snackbarClose = (event) =>{
+      this.setState({snackbaropen:false});
+    };
+
+    handleSubmit(event){
+      event.preventDefault();
+
+      fetch('https://localhost:44310/api/department',{
+        method:'PUT',
+        headers:{
+          'Accept':'application/json',
+          'Content-Type':'application/json'
+        },
+        body:JSON.stringify({
+          DepartmentID: event.target.DepartmentID.value,
+          DepartmentName: event.target.DepartmentName.value
+        })
+      })
+      .then(res=> res.json())
+      .then((result)=>
+      {
+        this.setState({snackbaropen:true, snackbarmsg:result});
+        //alert(result);
+      },
+      (error)=>
+      {
+        this.setState({snackbaropen:true, snackbarmsg:'Failed'});
+        //alert('Failed')
+      }
+      )
+      //alert(event.target.DepartmentName.value);
+    }
+
+    render(){
+        return(
+          <div className="container">
+            <Snakbar anchorOrigin={{vertical:'bottom',horizontal:'left'}}
+            open = {this.state.snackbaropen}
+            autoHideDuration = {2000}
+            onClose={this.snackbarClose}
+
+            message = {<span id="message-id">{this.state.snackbarmsg}</span>}
+            action={[
+              <IconButton
+                key = "close"
+                arial-label="Close"
+                color="inherit"
+                onClick={this.snackbarClose}
+                >
+                x
+              </IconButton>
+            ]}
+            />
+          <Modal
+          {...this.props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Edit Department
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          
+          
+          <Row>
+            <Col sm={6}>
+              <Form onSubmit={this.handleSubmit}>
+
+                <Form.Group controlId="DepartmentID">
+                  <Form.Label>DepartmentID</Form.Label>
+                  <Form.Control
+                  type="text"
+                  name="DepartmentID"
+                  required
+                  disabled
+                  defaultValue ={this.props.depid}
+                  placeholder="Department ID"
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="DepartmentName">
+                  <Form.Label>DepartmentName</Form.Label>
+                  <Form.Control
+                  type="text"
+                  name="DepartmentName"
+                  required
+                  defaultValue ={this.props.depname}
+                  placeholder="Department Name"
+                  />
+                </Form.Group>
+
+                <Form.Group>
+                    <Button variant="primary" type="submit">
+                      Update Department
+                    </Button>
+                </Form.Group>
+
+              </Form>
+            </Col>
+          </Row>
+
+          
+      </Modal.Body>
+        <Modal.Footer>
+          <Button variant="danger" onClick={this.props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+      </div>
+        );
+    }
+
+}
+
